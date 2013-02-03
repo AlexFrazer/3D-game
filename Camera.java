277cb17a04514a8ba3ecca8eacdd1a7f8c990751
Frazer <AlexADFrazer@gmail.com>
2013-02-03 09:08:04 -0800

    Create Camera.java

---
+++ Camera.java
@@ -0,0 +1,84 @@
+package Gaem;
+
+import javax.media.opengl.glu.GLU;
+
+public class Camera {
+      private double sqr(double d) {
+			return d*d;
+		}
+        double ex, ey, ez, cx, cy, cz, ux, uy, uz;
+        double theta = 0.087266463;
+        
+        Camera(double ex, double ey, double ez, double cx, double cy, double cz, double ux, double uy, double uz) {
+            this.ex = ex;
+            this.ey = ey;
+            this.ez = ez;
+            this.cx = cx;
+            this.cy = cy;
+            this.cz = cz;
+            this.ux = ux;
+            this.uy = uy; 
+            this.uz = uz;
+        }
+        
+        void setLookAt(GLU glu) {
+            glu.gluLookAt(ex, ey, ez, cx, cy, cz, ux, uy, uz);
+        }
+        
+        void forward() {
+            double dx = cx - ex, dy = cy - ey, dz = cz - ez;
+            double m = Math.sqrt(sqr(dx)+sqr(dy)+sqr(dz));
+            dx /= m/2;
+            dy /= m/2;
+            dz /= m/2;
+            
+            cx += dx;
+            cy += dy;
+            cz += dz;
+            ex += dx;
+            ey += dy;
+            ez += dz;
+        }
+		void back() {
+            double dx = cx - ex, dy = cy - ey, dz = cz - ez;
+            double m = Math.sqrt(sqr(dx)+sqr(dy)+sqr(dz));
+            dx /= m/2;
+            dy /= m/2;
+            dz /= m/2;
+            
+            cx -= dx;
+            cy -= dy;
+            cz -= dz;
+            ex -= dx;
+            ey -= dy;
+            ez -= dz;
+        }
+        
+        void left() {
+            double dx = cx - ex, dy = cy - ey, dz = cz - ez;
+            double m = Math.sqrt(sqr(dx)+sqr(dy)+sqr(dz));
+            dx /= m;
+            dy /= m;
+            dz /= m;
+            double nx = Math.cos(theta)*dx + Math.sin(theta)*dz;
+            double ny = dy;
+            double nz = -Math.sin(theta)*dx + Math.cos(theta)*dz;
+            cx = ex+nx;
+            cy = ey+ny;
+            cz = ez+nz;
+        }
+
+        void right() {
+            double dx = cx - ex, dy = cy - ey, dz = cz - ez;
+            double m = Math.sqrt(sqr(dx)+sqr(dy)+sqr(dz));
+            dx /= m;
+            dy /= m;
+            dz /= m;
+            double nx = Math.cos(theta)*dx - Math.sin(theta)*dz;
+            double ny = dy;
+            double nz = Math.sin(theta)*dx + Math.cos(theta)*dz;
+            cx = ex+nx;
+            cy = ey+ny;
+            cz = ez+nz;
+        }
+    }
