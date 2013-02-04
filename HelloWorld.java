fe210f1919147b65040b621a7acea3487f8997e1
Frazer <AlexADFrazer@gmail.com>
2013-02-04 10:39:48 -0800

    Create HelloWorld.java
    Just messing around with threads

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld implements Runnable {
+  String str;
+	long pause;
+	int index;
+	HelloWorld(String str, long pause) {
+		this.str=str;
+		this.pause=pause;
+		index=0;
+	}
+	public void run() {
+		try {
+			while(true) {
+				if(index > str.length()-1) break;
+				System.out.print(str.substring(index, index+1));
+				if(!str.substring(index, index+1).equals(" ")) Thread.sleep(pause);
+				index++;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
