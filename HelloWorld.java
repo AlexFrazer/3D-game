2e2c06d3a393179585789906d0e86bdf984cbbf0
Frazer <AlexADFrazer@gmail.com>
2013-02-04 18:01:36 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class HelloWorld implements Runnable {
 	String str;
 	long pause;
@@ -22,18 +20,7 @@ public class HelloWorld implements Runnable {
 		}
 	}
 	public static void main(String[] args) {
-		String genomeString="A";
-		int stringSize=10000;
-		Random r = new Random();
-		for(int i=0; i<stringSize; i++) {
-			int current=r.nextInt(3);
-			if(current==0) genomeString=genomeString + "A";
-			if(current==1) genomeString=genomeString + "T";
-			if(current==2) genomeString=genomeString + "G";
-			if(current==3) genomeString=genomeString + "C";
-		}
-		//System.out.println(genomeString);
-		Thread t = new Thread(new HelloWorld(genomeString, 10));
+		Thread t = new Thread(new HelloWorld("Hello World", 150));
 		t.start();
 	}
 }
